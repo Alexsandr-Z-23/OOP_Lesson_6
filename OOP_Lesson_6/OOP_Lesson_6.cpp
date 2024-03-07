27612acea1f9887bf808948c9c78084da5ba1ca5
Alexsandr <alex.zelik.m@bk.ru>
2024-03-07 21:19:22 +0300

    Новый класс

--- OOP_Lesson_6/OOP_Lesson_6.cpp
+++ OOP_Lesson_6/OOP_Lesson_6.cpp
@@ -1,91 +1,107 @@
-﻿#include <iostream>
-using namespace std;
-
-class Counter
-{
-public:
-	int value;
-	Counter(int num) :value{ num } {}
-	void print()
-	{
-		cout << "Value = " << value << endl;
-	}
-	//Counter operator +(const Counter& count)const   // перегрузка оператора "+" с обьектами
-	//{
-	//	return Counter{ value + count.value };
-	//}
-
-	//int operator +(int number) const   // перегрузка оператора "+" с значением
-	//{
-	//	return number+ value + number;
-	//}
-
-	 //operator int()const { return value; }  оператор неявного преобразования типа в int 
-	 //explicit operator int()const { return value; }  оператор явного преобразования типа в int
-
-	bool operator == (const Counter& num)const = default;
-	/*{
-		return value == num.value;
-	}*/
-	bool operator !=(const Counter& num)const
-	{
-		return value != num.value;
-	}
-	bool operator >(const Counter& num)const
-	{
-		return value > num.value;
-	}
-	bool operator <(const Counter& num)const
-	{
-		return value < num.value;
-	}
-
-};
-
-Counter operator + (const Counter& n1, const Counter& n2)  // перегрузка оператора "+" с обьектами
-{
-	return Counter{ n1.value + n2.value };
-}
-
-
-int main()
-{
-	//Counter count{20};
-	//int n= static_cast<int>( count);// преобразование от класса Counter в int
-	////cout << " INT = " << n << endl;
-	////int m = count;// не пройзойдет явное преобразование 
-	////cout << " INT = " << m<< endl;
-
-	/*Counter num1{20};
-	Counter num2{15};
-	Counter num3  { num1 + num2 };
-	int b = 100+num3+100 ;
-	num3.print();
-	cout << b;*/
-
-	Counter num1{20};
-	Counter num2{15};
-	bool n1 = num1 == num2;
-	bool n2 = num1 > num2;
-
-	cout << " num1 == num2  = " << boolalpha << n1 << endl;
-	cout << " num1 > num2  = " << boolalpha << n2 << endl;
-
-}
-
-//Операторы: 
+﻿//#include <iostream>
+//using namespace std;
 //
-//= присвоение
-//([]) индексирование						// внутри класса обьявление
-//(()) вызова
-//->  доступ к члену класса по указателю
-//по ситуации 
-//++i
-//--i
-//----------------------------------------------------------------
-//за пределами класса
-//new
-//new[]
-//delete
-//delete[]
-
+//class Counter
+//{
+//public:
+//	int value;
+//	Counter(int num) :value{ num } {}
+//	Counter() {}
+//	void print()
+//	{
+//		cout << "Value = " << value << endl;
+//	}
+//	//Counter operator +(const Counter& count)const   // перегрузка оператора "+" с обьектами
+//	//{
+//	//	return Counter{ value + count.value };
+//	//}
+//
+//	//int operator +(int number) const   // перегрузка оператора "+" с значением
+//	//{
+//	//	return number+ value + number;
+//	//}
+//
+//	 //operator int()const { return value; }  оператор неявного преобразования типа в int 
+//	 //explicit operator int()const { return value; }  оператор явного преобразования типа в int
+//
+//	bool operator == (const Counter& num)const = default;
+//	/*{
+//		return value == num.value;
+//	}*/
+//	bool operator !=(const Counter& num)const
+//	{
+//		return value != num.value;
+//	}
+//	bool operator >(const Counter& num)const
+//	{
+//		return value > num.value;
+//	}
+//	bool operator <(const Counter& num)const
+//	{
+//		return value < num.value;
+//	}
+//	friend ostream& operator<<(ostream& output, const Counter& num)
+//	{
+//		output << "Значение поля обьекта :" << num.value << endl;
+//		return output;
+//	}
+//	friend istream& operator>>(istream& input,  Counter& num)
+//	{
+//		input >> num.value;
+//	
+//		return input;
+//	}
+//};
+//
+//Counter operator + (const Counter& n1, const Counter& n2)  // перегрузка оператора "+" с обьектами
+//{
+//	return Counter{ n1.value + n2.value };
+//}
+//
+//
+//int main()
+//{
+//	//Counter count{20};
+//	//int n= static_cast<int>( count);// преобразование от класса Counter в int
+//	////cout << " INT = " << n << endl;
+//	////int m = count;// не пройзойдет явное преобразование 
+//	////cout << " INT = " << m<< endl;
+//
+//	/*Counter num1{20};
+//	Counter num2{15};
+//	Counter num3  { num1 + num2 };
+//	int b = 100+num3+100 ;
+//	num3.print();
+//	cout << b;*/
+//	setlocale(LC_ALL, "rus");
+//	Counter num1{20};
+//	Counter num2{15};
+//	bool n1 = num1 == num2;
+//	bool n2 = num1 > num2;
+//
+//	cout << " num1 == num2  = " << boolalpha << n1 << endl;
+//	cout << " num1 > num2  = " << boolalpha << n2 << endl;
+//
+//	cout << num1;
+//
+//	Counter num3{0};
+//	cin >> num3;
+//	cout << num3;
+//}
+//
+////Операторы: 
+////
+////= присвоение
+////([]) индексирование						// внутри класса обьявление
+////(()) вызова
+////->  доступ к члену класса по указателю
+////по ситуации 
+////++i
+////--i
+////----------------------------------------------------------------
+////за пределами класса
+////new
+////new[]
+////delete
+////delete[]
+//
---
+++ Ration_drob/Ration_drob.cpp
@@ -0,0 +1,64 @@
+﻿//Cсоздайте класс Ration который представляет дробь. Определить в нем переменные 
+//которые представляют числитель и знаменатель. также определить для этого класса операторы сложения и умножения 
+//а также операроры вода/вывода.
+
+#include <iostream>
+using namespace std;
+
+class Ration
+{
+	int numerator;
+	int denominator;
+
+public:
+	Ration(int num, int dnum)
+	{
+		numerator = num;
+		if (dnum == 0)
+		{
+			cout << "Знаменатель не может быть [ 0 ] !!! " << endl;
+			dnum = 1;
+		}
+		denominator = dnum;
+	};
+	Ration operator +(const Ration& drob)const
+	{
+
+		int dnum{ denominator };
+		int num{ numerator + drob.numerator };
+		if (denominator != drob.denominator)
+		{
+			num = numerator * drob.denominator + drob.numerator * denominator;
+			dnum = denominator * drob.denominator;
+		}
+		return Ration{ num,dnum };
+	}
+	Ration operator*(const Ration& drob)const
+	{
+		int dnum{ denominator * drob.denominator };
+		int num{ numerator * drob.numerator };
+		return Ration{ num,dnum };
+	}
+
+	int getNumerator()const { return numerator; }
+	int getDenominator() const { return denominator; }
+	//~Ration();
+};
+
+inline ostream& operator<<(ostream& stream, const Ration& drob)
+{
+	stream << drob.getNumerator() << " / " << drob.getDenominator() << endl;
+	return stream;
+}
+
+
+
+int main()
+{
+	Ration r1{ 2,3 };
+	Ration r2{ 5,8 };
+	Ration r3{ r1 * r2 };
+	cout << r3;
+}
+
+
